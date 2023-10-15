f61c676f0e2a1a5d3f31ff305adbf94a6364f804
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2023-10-15 01:31:43 -0400

    props

--- src/App.js
+++ src/App.js
@@ -4,58 +4,95 @@ function getTitle(title) {
   return title;
 }
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-function List() {
+function List(props) {
   return (
-    <ul>
-      {list.map((item) => (
-        <li key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span> by {item.author}</span>
-          <span>({item.num_comments})</span>
-          <span>({item.points})</span>
-        </li>
-      ))}
-    </ul>
+    <div>
+      {/* <h2>{props.title}</h2> */}
+      <ul>
+        {props.list.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
   );
 }
 
+const Item = ({ item }) => {
+  // const { item } = props;
+  return (
+    <li key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span> by {item.author}</span>
+      <span>({item.num_comments})</span>
+      <span>({item.points})</span>
+    </li>
+  );
+};
+
 function Search() {
+  const handleChange = (event) => console.log(event);
+  const handleMouseOver = (event) => console.log(event);
   return (
     <>
       <lable htmlFor="search">Search: </lable>
-      <input id="search" type="text"></input>
+      <input
+        id="search"
+        type="text"
+        onChange={handleChange}
+        onMouseOver={handleMouseOver}
+      ></input>
     </>
   );
 }
 
 function App() {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  // const javascriptLibraries = [
+  //   {
+  //     title: "jQuery",
+  //     url: "https://jquery.org/",
+  //     author: "John Resig",
+  //     num_comments: 3,
+  //     points: 4,
+  //     objectID: 0,
+  //   },
+  //   {
+  //     title: "Angular",
+  //     url: "https://angularjs.org/",
+  //     author: "Google",
+  //     num_comments: 2,
+  //     points: 5,
+  //     objectID: 1,
+  //   },
+  // ];
+
   return (
     <div>
       <h1>Hello, {getTitle("React")}</h1>
       <Search />
       <hr />
-      <List />
+      <List list={stories} />
+      {/* <List list={javascriptLibraries} title="JavaScript Libraries" /> */}
     </div>
   );
 }
